581446bc11b937eb9ec53511417e0555b0e565cc
jenshaw86 <bloo.j.shaw@gmail.com>
2019-10-14 11:22:03 -0700

    Revert "isUnique, first two approaches"
    This reverts commit 2f2918a0feb5ee7a01221343efdf4a89b4d24207.

--- 01-arrays-and-strings/01-is-unique.test.js
+++
@@ -1,9 +0,0 @@
-const isUnique = require('./01-is-unique');
-
-it("is unique", () => {
-  expect(isUnique('superman')).toBe(true)
-})
-
-it("is not unique", () => {
-  expect(isUnique('alphabet')).toBe(false)
-})
